eb8493611388bb444212c8ef52abcd2666f5e95d
radin-reth <radin.reth@gmail.com>
2014-09-06 10:08:45 +0700

    polish style homepage

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <html>
 <head>
 	<title></title>
+	<link href='http://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" type="text/css" href="bootstrap-3.2.0/css/bootstrap.css">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
@@ -13,24 +14,30 @@
 			</ul>
 		</div>
 	</nav>
+	<div class="bluepane">
+		<div class="container">
+			<h1 class="heading-name text-center">QR CODES</h1>
+			<em class="heading-description text-center">the most popular technology to bridge the physical-digital gap</em>
+	</div>
+	</div>
 	<div class="main-wrapper">
 		<div class="container">
 			<div class="row">
 				<div class="col-xs-12 col-sm-7 col-md-offset-1">
 					<form role="form">
 					  <div class="form-group">
-					    <label for="exampleInputEmail1">First Name:</label>
-					    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="First Name">
+					    <label for="exampleInputEmail1" class="lb-size">First Name:</label>
+					    <input type="text" class="form-control f-control-size" id="exampleInputEmail1" placeholder="First Name">
 					  </div>
 					  <div class="form-group">
-					    <label for="exampleInputEmail1">Last Name:</label>
-					    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Last Name">
+					    <label for="exampleInputEmail1" class="lb-size">Last Name:</label>
+					    <input type="text" class="form-control f-control-size" id="exampleInputEmail1" placeholder="Last Name">
 					  </div>
 					  <div class="form-group">
-					    <label for="exampleInputPassword1">Phone number:</label>
-					    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="phone">
+					    <label for="exampleInputPassword1" class="lb-size">Phone number:</label>
+					    <input type="text" class="form-control f-control-size" id="exampleInputPassword1" placeholder="phone">
 					  </div>
-					  <button type="submit" class="btn btn-default pull-right">Generate</button>
+					  <button type="submit" class="btn btn-success pull-right btn-lg">Generate</button>
 					</form>
 				</div>
 				<div class="col-xs-12 col-sm-4">
