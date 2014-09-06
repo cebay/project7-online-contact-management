e316926b71a88a09507541f827c464d1321e01aa
radin-reth <radin.reth@gmail.com>
2014-09-06 10:21:25 +0700

    add signin page

---
+++ signin/index.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+	<title></title>
+	<link href='http://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="../bootstrap-3.2.0/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
+</head>
+<body>
+	<div class="container">
+    <form role="form" class="form-signin">
+      <h2 class="form-signin-heading">Please sign in</h2>
+      <input type="email" autofocus="" required="" placeholder="Email address" class="form-control">
+      <input type="password" required="" placeholder="Password" class="form-control">
+      <label class="checkbox">
+        <input type="checkbox" value="remember-me"> Remember me
+      </label>
+      <button type="submit" class="btn btn-lg btn-primary btn-block">Sign in</button>
+    </form>
+  </div>
+  <script type="text/javascript" src="../bootstrap-3.2.0/js/bootstrap.js"></script>
+</body>
+</html>
