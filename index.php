4bf3ecfbf442a171e6249631b39e2ca3e56c89db
radin-reth <radin.reth@gmail.com>
2014-09-06 09:29:03 +0700

    center content

--- index.php
+++ index.php
@@ -13,32 +13,35 @@
 			</ul>
 		</div>
 	</nav>
-	<div class="container">
-		<div class="row">
-			<div class="col-xs-12 col-sm-7">
-				<form role="form">
-				  <div class="form-group">
-				    <label for="exampleInputEmail1">First Name:</label>
-				    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="First Name">
-				  </div>
-				  <div class="form-group">
-				    <label for="exampleInputEmail1">Last Name:</label>
-				    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Last Name">
-				  </div>
-				  <div class="form-group">
-				    <label for="exampleInputPassword1">Phone number:</label>
-				    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="phone">
-				  </div>
-				  <button type="submit" class="btn btn-default">Generate</button>
-				</form>
-			</div>
-			<div class="col-xs-12 col-sm-5">
-				<div class="qr-image">
-					
+	<div class="main-wrapper">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12 col-sm-7 col-md-offset-1">
+					<form role="form">
+					  <div class="form-group">
+					    <label for="exampleInputEmail1">First Name:</label>
+					    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="First Name">
+					  </div>
+					  <div class="form-group">
+					    <label for="exampleInputEmail1">Last Name:</label>
+					    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Last Name">
+					  </div>
+					  <div class="form-group">
+					    <label for="exampleInputPassword1">Phone number:</label>
+					    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="phone">
+					  </div>
+					  <button type="submit" class="btn btn-default pull-right">Generate</button>
+					</form>
+				</div>
+				<div class="col-xs-12 col-sm-4">
+					<div class="qr-image">
+
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
+
 	<script type="text/javascript" src="bootstrap-3.2.0/js/bootstrap.js"></script>
 </body>
 </html>
