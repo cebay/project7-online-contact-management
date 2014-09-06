0360c5e50f9bc62bb2e7244b1b455e1a8f55dc54
radin-reth <radin.reth@gmail.com>
2014-09-06 08:51:40 +0700

    initial homepage markup

--- index.php
+++ index.php
@@ -1,8 +1,23 @@
 <html>
 <head>
 	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
-	<h1>hello wor</h1>
+	<nav class="menu-top">
+		<div class="container">
+			<ul class="menu">
+				<li>login</li>
+				<li>signup</li>
+			</ul>
+		</div>
+	</nav>
+	<div class="container">
+		<form>
+			<input type="text" name="txt_fname">
+			<input type="text" name="txt_lname">
+			<input type="text" name="txt_phone">
+		</form>
+	</div>
 </body>
 </html>
