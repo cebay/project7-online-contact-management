b300200fefe5cfaaca8bf58f3c109bddae5ef2c6
radin-reth <radin.reth@gmail.com>
2014-09-06 16:09:28 +0700

    add manage member static page

---
+++ _admin/manage-member/index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+        <link rel="stylesheet" type="text/css" href="../../bootstrap-3.2.0/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="../css/main.css">
+
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <div class="container-full-width">
+            <div class="container mt-90">
+                <div class="main-content">
+                    <?php include("../partials/header.php"); ?>
+
+                    <!--main-->
+                    <div class="container">
+                        <div class="row">
+                          <!--left-->
+                            <div class="col-md-3" id="leftCol">
+                                <?php include("../partials/nav.php"); ?>
+                            </div><!--/left-->
+                          
+                          <!--right-->
+                          <div class="col-md-9">
+                            
+                            <table class="table table-striped">
+                              <thead>
+                                <tr>
+                                  <th>#</th>
+                                  <th>First Name</th>
+                                  <th>Last Name</th>
+                                  <th>Action</th>
+                                </tr>
+                              </thead>
+                              <tbody>
+                                <tr>
+                                  <td>1</td>
+                                  <td>Mark</td>
+                                  <td>Otto</td>
+                                  <td>@mdo</td>
+                                </tr>
+                                <tr>
+                                  <td>2</td>
+                                  <td>Jacob</td>
+                                  <td>Thornton</td>
+                                  <td>@fat</td>
+                                </tr>
+                                <tr>
+                                  <td>3</td>
+                                  <td>Larry</td>
+                                  <td>the Bird</td>
+                                  <td>@twitter</td>
+                                </tr>
+                              </tbody>
+                            </table>
+
+                          </div><!--/right-->
+                        </div><!--/row-->
+                    </div><!--/container-->
+                </div>
+            </div>
+        </div>
+
+        <script type="text/javascript" src="../js/jquery-1.10.2.min.js"></script>
+        <script src="../../bootstrap-3.2.0/js/bootstrap.min.js"></script>
+
+    </body>
+</html>
