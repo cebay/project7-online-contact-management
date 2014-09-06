d5ad9e820ba43e02144bc0c9438fde836f45b0e3
radin-reth <radin.reth@gmail.com>
2014-09-06 09:13:13 +0700

    add bootstrap form

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <html>
 <head>
 	<title></title>
+	<link rel="stylesheet" type="text/css" href="bootstrap-3.2.0/css/bootstrap.css">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
@@ -13,11 +14,29 @@
 		</div>
 	</nav>
 	<div class="container">
-		<form>
-			<input type="text" name="txt_fname">
-			<input type="text" name="txt_lname">
-			<input type="text" name="txt_phone">
-		</form>
+		<div class="row">
+			<div class="col-xs-12 col-sm-7">
+				<form role="form">
+				  <div class="form-group">
+				    <label for="exampleInputEmail1">First Name:</label>
+				    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="First Name">
+				  </div>
+				  <div class="form-group">
+				    <label for="exampleInputEmail1">Last Name:</label>
+				    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Last Name">
+				  </div>
+				  <div class="form-group">
+				    <label for="exampleInputPassword1">Phone number:</label>
+				    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="phone">
+				  </div>
+				  <button type="submit" class="btn btn-default">Generate</button>
+				</form>
+			</div>
+			<div class="col-xs-12 col-sm-5">
+				<img alt="140x140" class="img-rounded" data-src="holder.js/140x140" style="width: 140px; height: 140px;" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxNDAiIGhlaWdodD0iMTQwIj48cmVjdCB3aWR0aD0iMTQwIiBoZWlnaHQ9IjE0MCIgZmlsbD0iI2VlZSIvPjx0ZXh0IHRleHQtYW5jaG9yPSJtaWRkbGUiIHg9IjcwIiB5PSI3MCIgc3R5bGU9ImZpbGw6I2FhYTtmb250LXdlaWdodDpib2xkO2ZvbnQtc2l6ZToxMnB4O2ZvbnQtZmFtaWx5OkFyaWFsLEhlbHZldGljYSxzYW5zLXNlcmlmO2RvbWluYW50LWJhc2VsaW5lOmNlbnRyYWwiPjE0MHgxNDA8L3RleHQ+PC9zdmc+">
+			</div>
+		</div>
 	</div>
+	<script type="text/javascript" src="bootstrap-3.2.0/js/bootstrap.js"></script>
 </body>
 </html>
