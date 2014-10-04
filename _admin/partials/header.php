81314f812609cdfb82ec7f52dd541377e783d6f8
radin-reth <radin.reth@gmail.com>
2014-10-04 08:10:52 +0700

    remove login toggle button on session user_id is not exist

--- _admin/partials/header.php
+++ _admin/partials/header.php
@@ -6,14 +6,17 @@
     $auth_user = mysql_fetch_assoc($users);
 ?>
 
-
 <nav class="navbar navbar-default navbar-fixed-top" role="banner">
   <div class="container">
+    
     <div class="navbar-header">
         <a href="<?php echo $path; ?>">
             <img width="60" src="<?php echo $path; ?>../images/logo.png" id="logo" class="pull-left">
         </a>
         <a href="../" class="navbar-brand" id="listing-name"> Online Contact Management </a>
+        <?php 
+            if(isset($_SESSION['user_id'])) {
+        ?>
         <ul class="nav nav-tabs pull-right visible-xs">
             <li class="dropdown">
             <a class="dropdown-toggle" data-toggle="dropdown" href="#"><?php echo $auth_user['user_name']; ?> <b class="caret"></b></a>
@@ -24,8 +27,14 @@
             </ul>
             </li>
         </ul>
+        <?php
+        }
+        ?>
     </div>
     <nav class="collapse navbar-collapse" role="navigation">
+        <?php 
+            if(isset($_SESSION['user_id'])) {
+        ?>
         <ul class="nav nav-tabs pull-right nav-config">
             <li class="dropdown">
             <a class="dropdown-toggle" data-toggle="dropdown" href="#"><?php echo $auth_user['user_name']; ?> <b class="caret"></b></a>
@@ -36,6 +45,10 @@
             </ul>
             </li>
         </ul>
+        <?php
+            }
+        ?>
     </nav>
+    
   </div>
 </nav>
